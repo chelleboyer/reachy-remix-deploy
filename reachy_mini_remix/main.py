6aa4995fc8710f306f9b386a7bbc8fa6ddf6ff80
Michelle Boyer <chelleboyer@comcast.net>
2025-12-14 09:08:37 -0600

    Add file-based debug logging to prove run() is called

--- reachy_mini_remix/main.py
+++ reachy_mini_remix/main.py
@@ -28,9 +28,15 @@ class ReachyMiniRemix(ReachyMiniApp):
         """
         import sys
         import logging
+        import time
+        
+        # CRITICAL: Test if run() is even being called
+        with open("/tmp/reachy-remix-debug.log", "w") as f:
+            f.write("RUN METHOD CALLED!\n")
+            f.flush()
         
         # Set up logging to help debug
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO, force=True)
         logger = logging.getLogger(__name__)
         
         logger.info("=" * 60)
